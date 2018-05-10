6ef032fc3ab41190c08bc3768ba4a1c9beb293ef
Patrick Cullen <patrick47cullen@gmail.com>
2018-05-10 11:47:11 +0100

    Adds Tool abstract class and associated tests

--- src/test/java/ToolTest.java
+++ src/test/java/ToolTest.java
@@ -1,12 +1,28 @@
 import org.junit.Before;
+import org.junit.Test;
+import tools.Herb;
 import tools.Potion;
+
+import static org.junit.Assert.assertEquals;
 
 public class ToolTest {
 
     Potion magicPotion;
+    Herb magicHerb;
 
     @Before
     public void before(){
         magicPotion = new Potion();
+        magicHerb = new Herb();
+    }
+
+    @Test
+    public void canGetRecoveryPointsPotion(){
+        assertEquals(30, magicPotion.getHealthRecoveryPoints());
+    }
+
+    @Test
+    public void canGetRecoveryPointsHerb(){
+        assertEquals(15, magicHerb.getHealthRecoveryPoints());
     }
 }
